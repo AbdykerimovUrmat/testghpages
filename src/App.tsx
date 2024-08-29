c1cedb3539c9375f635ef2ff496e45aca4f68744
Urmat Abdykerimov <abdykerimov.urm@gmail.com>
2024-08-29 11:36:40 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
-function App() {
+
+
+export default function App() {
+
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          hi
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Character.ts
@@ -0,0 +1,5 @@
+export type Character = {
+    name: String,
+    id: String,
+    like: Boolean,
+};
